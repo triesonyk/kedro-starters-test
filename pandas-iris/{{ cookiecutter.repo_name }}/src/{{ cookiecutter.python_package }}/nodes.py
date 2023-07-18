659858d6c6db7ae611ce0c108b339fa450188b40
Trie Sony Kusumowibowo <triesonyk@gmail.com>
2023-07-18 14:48:34 +0700

    Update nodes.py

--- pandas-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/nodes.py
+++ pandas-iris/{{ cookiecutter.repo_name }}/src/{{ cookiecutter.python_package }}/nodes.py
@@ -9,6 +9,19 @@ from typing import Any, Dict, Tuple
 import numpy as np
 import pandas as pd
 
+def test_data(
+    data: pd.DataFrame, parameters: Dict[str, Any]
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    """Splits data into features and target training and test sets.
+
+    Args:
+        data: Data containing features and target.
+        parameters: Parameters defined in parameters.yml.
+    Returns:
+        Split data.
+    """
+    )
+    return data.info()
 
 def split_data(
     data: pd.DataFrame, parameters: Dict[str, Any]
